4ca15165cbed4f7583f454ceddfbe88474ae164c
Icemap <cheese.javaee@gmail.com>
2022-10-22 17:02:40 +0800

    [Update] add echarts-gl to support 3D charts

--- components/Tab/ResultEchartsTab.tsx
+++ components/Tab/ResultEchartsTab.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import ReactEcharts from 'echarts-for-react';
+import 'echarts-gl'
 import Typography from '@mui/material/Typography';
 
 export default function ResultEchartsTab(props: { data?: any }) {

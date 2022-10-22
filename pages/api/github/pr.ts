3c53ecdef143dfa2cd40ab1fc99f8f5ad9531df3
czhen <56986964+shczhen@users.noreply.github.com>
2022-10-22 12:13:44 +0800

    restrict czhen to create pr

--- pages/api/github/pr.ts
+++ pages/api/github/pr.ts
@@ -16,13 +16,18 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<any>
 ) {
-  // const session = await unstable_getServerSession(req, res, authOptions);
-  // if (!session) {
-  //   res.status(401).end();
-  //   return;
-  // }
-  // const { login } = await getUserDetails(session.accessToken);
-  const login = 'test';
+  const session = await unstable_getServerSession(req, res, authOptions);
+  if (!session) {
+    res.status(401).end();
+    return;
+  }
+  const { login } = await getUserDetails(session.accessToken);
+
+  if (!['shczhen', 'shczhen-bot'].includes(login)) {
+    res.status(401).end();
+    return;
+  }
+
   const { sql, js, option } = req?.body || {};
 
   await axios.post(

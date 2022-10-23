bfdcf677bb1a557beb801cd668549b851828466f
Icemap <cheese.javaee@gmail.com>
2022-10-23 14:31:47 +0800

    [Update] pie chart update

--- configs/panels/developer-events/render.js
+++ configs/panels/developer-events/render.js
@@ -1,19 +1,43 @@
 function main(data) {
   option = {
-    legend: {},
-    tooltip: {},
+    tooltip: {
+      trigger: 'item'
+    },
+    legend: {
+      show: false
+    },
     dataset: {
       dimensions: ['type', 'num'],
       source: data
     },
     xAxis: {
-      type: 'category',
-      axisLabel: {
-        rotate: 45
-      }
+      type: 'category'
     },
     yAxis: {},
-    series: [{ type: 'pie' }]
+    series: [{
+      type: 'pie',
+      radius: ['40%', '70%'],
+      avoidLabelOverlap: false,
+      itemStyle: {
+        borderRadius: 10,
+        borderColor: '#fff',
+        borderWidth: 2
+      },
+      label: {
+        show: false,
+        position: 'center'
+      },
+      emphasis: {
+        label: {
+          show: true,
+          fontSize: '20',
+          fontWeight: 'bold'
+        }
+      },
+      labelLine: {
+        show: false
+      }
+    }]
   };
 
   return option;
